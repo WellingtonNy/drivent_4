012f285e48fef612408dae0d46c4d104913fc312
WellingtonLinux <wellingtoncpfilho@gmail.com>
2023-07-24 06:48:56 -0300

    fix:middle error

--- src/middlewares/error-handling-middleware.ts
+++ src/middlewares/error-handling-middleware.ts
@@ -38,6 +38,13 @@ export function handleApplicationErrors(
     });
   }
 
+  if (err.name === 'reservaError') {
+    return res.status(httpStatus.FORBIDDEN).send({
+       message: err.message
+      })
+  }
+
+
   /* eslint-disable-next-line no-console */
   console.error(err.name);
   res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
